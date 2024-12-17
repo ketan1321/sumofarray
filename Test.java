978ce8b72d8cc47012dfac8c5e052f4683211232
ketan1321 <ketankawade10@gamail.com>
2024-12-17 15:32:35 +0530

    Adding Average of Array Feature

--- Test.java
+++ Test.java
@@ -2,9 +2,12 @@ public class Test {
     public static void main(String[] args) {
         int[] array1 = { 10, 20, 30, 40, 50 };
         int sum = 0;
+        int length_of_array = array1.length;
         for (int i = 0; i < array1.length; i++) {
             sum = sum + array1[i];
         }
         System.out.println("Sum Of Array = " + sum);
+        double average = sum / length_of_array;
+        System.out.println("Average Of All Array Element = " + average);
     }
 }
